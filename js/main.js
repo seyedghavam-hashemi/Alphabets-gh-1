b944d7eaae109caecba4a33d97b6bf5aa7515053
GHAVAM <80568373+seyedghavam-hashemi@users.noreply.github.com>
2024-07-02 22:55:09 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,39 +1,3 @@
-/*function funcCheckBox() {
-    var correct1 = document.getElementById("correct1");
-    var correct2 = document.getElementById("correct2");
-    var correct3 = document.getElementById("correct3");
-    var correct4 = document.getElementById("correct4");
-
-
-    let gateCloset = document.getElementById("gateCloset").innerHTML;
-    let gateClosep = document.getElementById("gateClosep").innerHTML;
-    let gateOpent = document.getElementById("gateOpent").innerHTML;
-    let gateOpenp = document.getElementById("gateOpenp").innerHTML;
-
-
- 
-
-
-
-    if (   correct1.checked == true && correct2.checked == true && correct3.checked == true && correct4.checked == true
-        ){
-            gateOpent.style.display = "block";
-            gateOpenp.style.display = "block";
-
-            gateCloset.style.display = "none";
-
-            gateClosep.style.display = "none";
-    } else {
-        gateOpent.style.display = "none";
-        gateOpenp.style.display = "none";
-
-        gateCloset.style.display = "block";
-        gateClosep.style.display = "block";
-
-    }
-  }
-  */
-
 
 
 
@@ -43,6 +7,18 @@
     var correct3 = document.getElementById("correct3");
     var correct4 = document.getElementById("correct4");
     var correct5 = document.getElementById("correct5");
+
+    var incorrect1 = document.getElementById("incorrect1");
+    var incorrect2 = document.getElementById("incorrect2");
+    var incorrect3 = document.getElementById("incorrect3");
+    var incorrect4 = document.getElementById("incorrect4");
+
+
+
+
+
+
+
 
 
     var gateCloset = document.getElementById("gateCloset");
@@ -58,6 +34,16 @@
         && correct3.checked == true
        && correct4.checked == true
         && correct5.checked == true
+
+        && incorrect1.checked == false
+        && incorrect2.checked == false
+       && incorrect3.checked == false
+        && incorrect4.checked == false
+
+
+
+
+
 
         ){
             gateOpent.style.display = "block";
